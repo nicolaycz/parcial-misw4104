137755d2ce84dee309987a8b232c04747af0b4c1
nicolay <nicolay.cc@outlook.com>
2026-04-28 21:39:36 -0500

    feat: configurar rutas maestro-detalle de repositorios

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarRepositorios } from './repositorio/listar-repositorios/listar-repositorios';
+import { DetalleRepositorio } from './repositorio/detalle-repositorio/detalle-repositorio';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'repositorios', component: ListarRepositorios },
+  { path: 'repositorios/:id', component: DetalleRepositorio },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
