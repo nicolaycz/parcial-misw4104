af51b25a84f6e89aa5c0b4bed06fdc011fc0dd29
nicolay <nicolay.cc@outlook.com>
2026-04-28 20:52:34 -0500

    feat: agregar modulo de repositorios

---
+++ src/app/repositorio/repositorio-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RepositorioRoutingModule } from './repositorio-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RepositorioRoutingModule],
+})
+export class RepositorioModule {}
