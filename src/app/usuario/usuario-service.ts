6eea181d464acf2318d30561cf6edd95ce1c308d
nicolay <nicolay.cc@outlook.com>
2026-04-28 21:13:47 -0500

    chore: configurar environments y mover urls de servicios

--- src/app/usuario/usuario-service.ts
+++ src/app/usuario/usuario-service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from './usuario';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  private apiUrl = 'https://gist.githubusercontent.com/caev03/628509e0b3fe41dd44f6a2ab09d81ef9/raw/f847eafbecca47287ff0faec4de1329b874f5711/users.json';
+  private apiUrl = environment.usuariosUrl;
 
   constructor(private http: HttpClient) { }
 
