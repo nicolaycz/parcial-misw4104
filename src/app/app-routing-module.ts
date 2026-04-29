af4d994b34cc07053943870b4c3be98e15e25df7
nicolay <nicolay.cc@outlook.com>
2026-04-28 21:40:15 -0500

    feat: agregar navegacion entre usuarios y repositorios

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarUsuarios } from './usuario/listar-usuarios/listar-usuarios';
 import { ListarRepositorios } from './repositorio/listar-repositorios/listar-repositorios';
 import { DetalleRepositorio } from './repositorio/detalle-repositorio/detalle-repositorio';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
+  { path: 'usuarios', component: ListarUsuarios },
   { path: 'repositorios', component: ListarRepositorios },
   { path: 'repositorios/:id', component: DetalleRepositorio },
 ];
